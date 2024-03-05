b3d7d2b6cd2ffa2ae4921b93df345a879e2b0829
Chirag Sharma <chirag@Chirags-MacBook-Pro.local>
2024-03-05 14:12:58 +0530

    changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,9 +1,15 @@
-import { MongoClient } from 'mongodb';
+import { MongoClient, ServerApiVersion } from 'mongodb';
 
 const handler = async (req, res)=>{
   const uri = process.env.MONGO_URI;
 
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
 
   try {
     await client.connect();
