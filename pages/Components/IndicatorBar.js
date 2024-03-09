a9863ace0ffbf28df7789109fbb5c2e197b6bf41
Chirag Sharma <chirag@Chirags-MacBook-Pro.local>
2024-03-09 17:00:07 +0530

    changes

--- pages/Components/IndicatorBar.js
+++ pages/Components/IndicatorBar.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import styles from './IndicatorBar.module.css';
 
-const IndicatorBar = ({ lowerValue, upperValue, normalRange }) => {
+const IndicatorBar = () => {
     
 
   return (
     <div className={styles.indicatorBar}>
-      <div className={styles.indicator} style={{ left: `${lowerPercentage}%`, width: `${normalPercentage}%` }}></div>
-      <div className={styles.lower} style={{ width: `${lowerPercentage}%` }}></div>
-      <div className={styles.upper} style={{ left: `${upperPercentage}%`, width: `${100 - upperPercentage}%` }}></div>
     </div>
   );
 };
